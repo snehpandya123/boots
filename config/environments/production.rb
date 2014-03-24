1a605c36bdfd90fcf3fc5a7ba1af5aa1eaa388f8
sneh <sapandya92@yahoo.co.in>
2014-03-24 22:57:04 +0530

    heroku mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,18 @@ Boot::Application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = {:host => 'http://blooming-hollows-3183.herokuapp.com/', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+     address: "smtp.gmail.com",
+     port: 587,
+     domain: "mail.google.com",####important
+     authentication: "plain",
+     enable_starttls_auto: true,
+     user_name: "snehp92@gmail.com",
+     password: "ernakulam;"
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
