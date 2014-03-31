75241b543bcab7254eed560011f32d86410c5bb9
sneh <sapandya92@yahoo.co.in>
2014-04-01 01:06:52 +0530

    jquery try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,10 +21,10 @@ Boot::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -34,7 +34,7 @@ Boot::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  #config.assets.version = '1.0'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -68,6 +68,8 @@ Boot::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
+config.assets.compile = ['*.js', '*.css']
+config.active_support.deprecation = :silence
   config.i18n.fallbacks = true
   config.action_mailer.default_url_options = { :host => 'http://blooming-hollows-3183.herokuapp.com/' }
   config.action_mailer.raise_delivery_errors = true
